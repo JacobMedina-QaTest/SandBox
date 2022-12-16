c8a68aa05849a45abeb58117b1c2b6b947d67a96
Jacob.Medina <jrmQaTest@gmail.com>
2022-12-15 19:56:04 -0500

    added sandbox print statement

--- SandBox/src/test/java/ToolBoxPackage/ToolBox.java
+++ SandBox/src/test/java/ToolBoxPackage/ToolBox.java
@@ -3,7 +3,7 @@ package ToolBoxPackage;
 public class ToolBox {
 
 	public static void main(String[] args) {
-	System.out.println("Just created a class");
+	System.out.println("Just created a sandbox class");
 
 	}
 
