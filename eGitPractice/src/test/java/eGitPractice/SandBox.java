1a95533b12a95e373af254cbf2338785cd4d26fc
thangjuice2 <xxthangh@gmail.com>
2022-12-15 20:27:32 -0500

    this is 2nd commit from thang

--- eGitPractice/src/test/java/eGitPractice/SandBox.java
+++ eGitPractice/src/test/java/eGitPractice/SandBox.java
@@ -4,7 +4,9 @@ public class SandBox {
 
 	public static void main(String[] args) {
 		System.out.println("This is Thang");
+		
+		System.out.println("This is new branch on thang");
+
 
 	}
-
 }
---
+++ eGitPractice/src/test/java/mergePractice/ThisIsMerging.java
@@ -0,0 +1,10 @@
+package mergePractice;
+
+public class ThisIsMerging {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
