6028ea84010a98ca8767471103be6a8b3a968960
Negash Debala <nagashfebala@gmail.com>
2022-12-29 13:38:57 -0500

    my second commit, i have created a action class

---
+++ src/test/java/chatGPTPackage/Nagash2.java
@@ -0,0 +1,12 @@
+package chatGPTPackage;
+
+public class Nagash2 {
+	public void walk() {
+		System.out.println("nagash is walking");
+	}
+
+	public static void main(String[] args) {
+		Nagash2 n = new Nagash2();
+		n.walk();
+	}
+}
