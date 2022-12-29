f3e24d7d20ae90832f4fb747e92c88b40f3ec98c
Negash Debala <nagashfebala@gmail.com>
2022-12-29 13:03:34 -0500

    commiting to local repo

---
+++ src/test/java/ToolBoxPackage/ToolBox.java
@@ -0,0 +1,11 @@
+package ToolBoxPackage;
+
+public class ToolBox {
+
+	public static void main(String[] args) {
+	System.out.println("Just created a sandbox class");
+	System.out.println("...");
+
+	}
+
+}
